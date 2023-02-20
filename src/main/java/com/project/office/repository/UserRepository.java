30421408cf03a811dd9ab45faf9dfa089dd30dd6
maskadev <sidkiyouness@gmail.com>
2023-02-20 19:07:00 +0100

    add user services and repo

---
+++ src/main/java/com/project/office/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.project.office.repository;
+
+import com.project.office.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
