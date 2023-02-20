23e4d22f94aa9904636defa5633acb640dc42609
maskadev <sidkiyouness@gmail.com>
2023-02-20 18:29:34 +0100

    remove spacing

--- src/main/java/com/project/office/OfficeApplication.java
+++ src/main/java/com/project/office/OfficeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OfficeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OfficeApplication.class, args);
 	}
