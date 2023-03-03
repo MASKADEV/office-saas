500a10d071a86f083fcf46959f9c802d13707996
maskadev <sidkiyouness@gmail.com>
2023-03-03 10:21:25 +0100

    update workflow

--- src/main/java/com/project/office/config/RedisConfig.java
+++ src/main/java/com/project/office/config/RedisConfig.java
@@ -6,14 +6,11 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.repository.support.RedisRepositoryFactoryBean;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.stereotype.Repository;
 
 @Configuration
-@EnableRedisRepositories(basePackages = "com.project.office.config", repositoryFactoryBeanClass = RedisRepositoryFactoryBean.class, redisTemplateRef = "redisTemplate", keyPrefix = "user")
-@Repository("redisUserRepository")
+@EnableRedisRepositories
 public class RedisConfig {
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
